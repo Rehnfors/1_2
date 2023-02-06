bdb76243593bb4fb44b4552b39f5a16e736b3270
Jesper Rehnfors <rehnforsjesper@gmail.com>
2023-02-06 07:53:59 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "deck.hh"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+     Deck deck = Deck();
+    cout << "Fresh deck: " << deck << endl;
+    deck.shuffle();
+    cout << "Shuffled deck: " << deck << endl;
+    deck.sort();
+    cout << "Sorted deck: " << deck << endl;
+    return 0;
+}
