6da5487656ad3c882c1c23eea69494000f41ef9c
Jesper Rehnfors <rehnforsjesper@gmail.com>
2023-02-10 12:08:49 +0100

    update

--- deck.cpp
+++ deck.cpp
@@ -4,23 +4,25 @@
 #include <string>
 #include <algorithm>
 #include <random>
-#include <chrono> 
+#include <chrono>
 
 using namespace std;
 
 /**
  * Help function to convert Suit enum to string
-*/
-const string SuitToString(Suit s) {
-    switch (s) {
-        case Suit::Hearts:
-            return "H";
-        case Suit::Diamonds:
-            return "D";
-        case Suit::Spades:
-            return "S";
-        case Suit::Clubs:
-            return "C";
+ */
+const string SuitToString(Suit s)
+{
+    switch (s)
+    {
+    case Suit::Hearts:
+        return "H";
+    case Suit::Diamonds:
+        return "D";
+    case Suit::Spades:
+        return "S";
+    case Suit::Clubs:
+        return "C";
     }
     return "";
 }
@@ -33,26 +35,27 @@ Card::Card(int rank, Suit suit)
 
 /**
  * Overload of << operator for Card class
-*/
-ostream & operator << (ostream &out, const Card &c)
+ */
+ostream &operator<<(ostream &out, const Card &c)
 {
     string r;
-    switch(c.rank) {
-        case 1:
-            r = 'A';
-            break;
-        case 11:
-            r = 'J';
-            break;
-        case 12:
-            r = 'Q';
-            break;
-        case 13:
-            r = 'K';
-            break;
-        default:
-            r = to_string(c.rank);
-            break;
+    switch (c.rank)
+    {
+    case 1:
+        r = 'A';
+        break;
+    case 11:
+        r = 'J';
+        break;
+    case 12:
+        r = 'Q';
+        break;
+    case 13:
+        r = 'K';
+        break;
+    default:
+        r = to_string(c.rank);
+        break;
     }
 
     out << r << SuitToString(c.suit);
@@ -62,10 +65,13 @@ ostream & operator << (ostream &out, const Card &c)
 
 /**
  * Create a deck of 52 cards
-*/
-Deck::Deck() {
-    for (Suit s = Suit::Hearts; s <= Suit::Clubs; s = Suit(s + 1)) {
-        for (int r=1; r<14; r++) {
+ */
+Deck::Deck()
+{
+    for (Suit s = Suit::Hearts; s <= Suit::Clubs; s = Suit(s + 1))
+    {
+        for (int r = 1; r < 14; r++)
+        {
             cards.push_back(Card(r, s));
         }
     }
@@ -73,11 +79,12 @@ Deck::Deck() {
 
 /**
  * Overload of << operator for Deck class
-*/
-ostream & operator << (ostream &out, const Deck &d)
+ */
+ostream &operator<<(ostream &out, const Deck &d)
 {
     string separator;
-    for (auto i: d.cards) {
+    for (auto i : d.cards)
+    {
         out << separator << i;
         separator = ", ";
     }
@@ -85,67 +92,75 @@ ostream & operator << (ostream &out, const Deck &d)
     return out;
 }
 
-/** 
+/**
  * Return number of cards in deck
- * 
-*/
+ *
+ */
 int Deck::size()
 {
     return this->cards.size();
 }
 
 /**
- * Shuffle all the cards in deck in ranom order. 
-*/
+ * Shuffle all the cards in deck in ranom order.
+ */
 void Deck::shuffle()
 {
     auto rng = std::default_random_engine(std::chrono::system_clock::now().time_since_epoch().count());
-    std::shuffle(this->cards.begin(),this->cards.end(), rng);
+    std::shuffle(this->cards.begin(), this->cards.end(), rng);
 }
 
 /**
  * Sort the deck according to card rank. All aces, the all two's, then all threes, ...
-*/
+ */
 void Deck::sort()
 {
     vector<Card> sorted_cards; // Empty temporary deck
 
-    while (this->size() > 0) {
+    while (this->size() > 0)
+    {
         // Go through the deck from left to right, insert the deck in the appropriate spot
-        Deck::insert(sorted_cards, this->take()); //Take a card from the old deck, insert it into the new one
+        Deck::insert(sorted_cards, this->take()); // Take a card from the old deck, insert it into the new one
     }
 
     this->cards = sorted_cards;
 }
 
 /**
- * Take the top card from the deck 
-*/
-Card Deck::take() {
+ * Take the top card from the deck
+ */
+Card Deck::take()
+{
     Card c = this->cards.back();
     this->cards.pop_back();
     return c;
 }
 
 /**
- * Put a card on top of the deck 
-*/
-void Deck::put(Card card) {
+ * Put a card on top of the deck
+ */
+void Deck::put(Card card)
+{
     cards.push_back(card);
 }
 
+void Deck::insert(vector<Card> &cardlist, Card card)
+{
+    cout << "korv 0 " << endl;
 
-
-void Deck::insert(vector<Card> &cardlist, Card card) {
-
-    for(int i = 0; i < cardlist.size(); i++)
+    for (int i = 0; i < cardlist.size(); i++)
     {
+
+        card = cardlist[i];
         int j = i;
-        while(j > 0 && cardlist[j-1] > cardlist[j])
+        cout << "korv 1" << endl;
+
+        while (j >= 0 && cardlist[j] > card)
         {
-            card = cardlist[j-1];
-            cardlist[j-1] = cardlist[j];
-            cardlist[j] = card;
+            cardlist[j + 1] = cardlist[j];
+            j--;
+            cout << "korv 2" << endl;
         }
+        cardlist[j + 1] = card;
     }
 }
